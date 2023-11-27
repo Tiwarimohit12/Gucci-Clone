07452b5c3c6f0d233605f884ba0d00261d40fb8f
Mohit Tiwari <145648854+Tiwarimohit12@users.noreply.github.com>
2023-11-27 23:25:51 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,155 @@
+gsap.to("#main svg",{
+    y:-120,
+    top:30,
+    scrub:3,
+    scale:0.2,
+    duration:0.5,
+    delay:0.4
+})
+gsap.to("#page2 #div2",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:0.8,
+})
+gsap.to("#page3 #div1",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:1.6,
+})
+gsap.to("#page3 #div2",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:2,
+})
+gsap.to("#page4 #div1",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:2.4,
+})
+gsap.to("#page4 #div2",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:2.8,
+})
+gsap.to("#page4 h1",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:3.2,
+})
+gsap.to("#page4 #div3",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:3.6,
+})
+gsap.to("#page5 #div1 img",{
+    // x:240,
+    y:-130,
+    top:30,
+    scrub:3,
+    // opacity:0,
+    duration:0.5,
+    delay:4,
+})
+gsap.to("#page6 #div1",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:4.4,
+})
+gsap.to("#page6 #div2",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:4.8,
+})
+gsap.to("#page7 #div1",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:5.2,
+})
+gsap.to("#page7 #div2",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:5.6,
+})
+gsap.to("#page8",{
+    y:-130,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:5.8,
+})
+gsap.to("#page8 #div1 h1",{
+    y:-10,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:6,
+})
+gsap.to("#page8 #div2 h1",{
+    y:-10,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:6.4,
+})
+gsap.to("#page8 #div3 h1",{
+    y:-10,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:6.8,
+})
+gsap.to("#page8 ",{
+    y:-10,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:6.8,
+})
+gsap.to("#page9 svg",{
+    y:-10,
+    top:30,
+    scrub:3,
+    // scale:0.2,
+    duration:0.5,
+    delay:6.8,
+})
+
+
